3e2356058dc1e4763d3ed5a1e963d0476b70ae93
parthivsaikia <parthivsaikia985@gmail.com>
2025-02-16 23:10:26 +0530

    add the file uploader

--- src/components/fileUploader.tsx
+++ src/components/fileUploader.tsx
@@ -1,12 +1,50 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+type UploadStatus = "idle" | "uploading" | "success" | "error";
 
 const FileUploader = () => {
-  const [file, setFile] = useState<File | null>(null);
-  return (
-    <div>
-      <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)} />
-    </div>
-  );
+  const [status, setStatus] = useState<UploadStatus>("idle");
+    const [file, setFile] = useState<File | null>(null);
+    const [summary, setSummary] = useState("")
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault()
+        if (!file) {
+            return 
+        }
+        setStatus("uploading")
+        const formData = new FormData()
+        formData.append('file', file)
+        try {
+            const response = await axios.post('https://parthivsaikia-neurathon-backend.hf.space/analyze/', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+            })
+            setSummary(response.data.summary)
+            setFile(null)
+            setStatus("success")
+        } catch {
+            setStatus('error')
+        }
+
+    }
+    return (
+      <div>
+        <form>
+          <input type="file" onChange={handleFileChange} />
+          {file && status !== "uploading" && (
+            <button onClick={handleSubmit}>Upload</button>
+          )}
+        </form>
+        <div>{summary}</div>
+      </div>
+    );
 };
 
 export default FileUploader;
