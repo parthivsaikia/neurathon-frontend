c3536c3ccf370422489de496155d284454ec3e3a
parthivsaikia <parthivsaikia985@gmail.com>
2025-02-20 22:24:12 +0530

    change the frontend ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import "./index.css";
 import FileUploader from "./components/fileUploader";
-
+import GradientText from "./blocks/TextAnimations/GradientText/GradientText";
 const App = () => {
-
-  return <div>
-    <FileUploader/>
-  </div>;
+  return (
+    <div>
+      <GradientText
+        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+        animationSpeed={3}
+        showBorder={false}
+        className="custom-class text-8xl"
+      >
+        <h1>Add a splash of color!</h1>
+      </GradientText>
+      <div className="flex justify-center items-center h-screen">
+        <FileUploader />
+      </div>
+    </div>
+  );
 };
 
 export default App;
